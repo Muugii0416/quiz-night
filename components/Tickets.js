6f03ad8417fbb59d712f5179dcd0e82ef9af05a6
munkhtur <80816938+Munkhtur89@users.noreply.github.com>
2024-08-16 12:43:20 +0800

    ticket

--- components/Tickets.js
+++ components/Tickets.js
@@ -63,7 +63,10 @@ const Tickets = () => {
             </div>
             <div className="text-center p-2 xs:text-xl">Үнэ : 100,000₮</div>
             <div className="text-center p-2 xs:text-xl">
-              <Link href="https://www.quiznight.mn/" passHref>
+              <Link
+                href="https://www.portal.mn/quiznight/66b19f26d290ccac18731044"
+                passHref
+              >
                 <Button
                   className="bg-gradient-to-r from-[#B75CFF] to-[#671AE4] w-full lg:w-[400px] mt-3 flex-1 justify-center"
                   as="a"
@@ -104,7 +107,10 @@ const Tickets = () => {
             </div>
             <div className="text-center p-2 xs:text-xl">Үнэ : 100,000₮</div>
             <div className="text-center p-2 xs:text-xl">
-              <Link href="https://www.quiznight.mn/" passHref>
+              <Link
+                href="https://www.portal.mn/quiznight/66b19dcad290ccac1873102d"
+                passHref
+              >
                 <Button
                   className="bg-gradient-to-r from-[#B75CFF] to-[#671AE4] w-full lg:w-[400px] mt-3 flex-1 justify-center"
                   as="a"
@@ -121,6 +127,3 @@ const Tickets = () => {
 };
 
 export default Tickets;
-
-// https://www.portal.mn/quiznight/66b19dcad290ccac1873102d
-// https://www.portal.mn/quiznight/66b19f26d290ccac18731044
